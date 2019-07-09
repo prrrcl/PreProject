05f935122e3aa4dff370c0c369f52f5615162c0a
prrrcl <adrianporcelnavarro@hotmail.com>
2019-07-09 11:02:26 +0200

    Layout.js estructured

--- Layout.js
+++ Layout.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function Layout(root,style){
+
+  this.root = root;
+  this.style = style;
+
+}
+
+Layout.prototype.generate = function(){
+  // Generamos elementos del dom
+}
+Layout.prototype.render = function(){
+  // Añadir los elementos generados al DOM
+}
+Layout.prototype.getContainers = function(){
+  // Obtener los elementos del dom: Header, main y footer del dom
+}
--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-'use strict'
+'use strict';
+
+function main() {
+  const ENTRY_POINT = '/';
+  let layoutInstance = null;
+
+generateLayout();
+
+  function generateLayout(){
+    layoutInstance = new layout();
+    layoutInstance.generate();
+  }
+
+}
+
+window.addEventListener('load', main);
